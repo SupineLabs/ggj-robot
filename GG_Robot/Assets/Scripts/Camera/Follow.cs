6a8f55503d9a0a5d1d8f8c3f9a4caa1b31008e6e
Jack Brewer <jack.brewer@students.plymouth.ac.uk>
2020-01-31 22:05:14 +0000

    S1C2 - Movement system and camera done

---
+++ GG_Robot/Assets/Scripts/Camera/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private float _smoothing;
+
+    void FixedUpdate()
+    {
+        Vector3 newPos = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, (_smoothing * 0.01f));
+    }
+}
