285a1b9362ea4d974ce6d137977b570666ab6489
Alex Pritchard <alexpritc@users.noreply.github.com>
2020-01-31 20:25:17 +0000

    Alex's initial commit.
    - Camera follows player.
    - Player moves on X axis.
    - Player teleports to new scene when enters trigger.

---
+++ GG_Robot/Assets/Scripts/Alex_TempMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alex_TempMove : MonoBehaviour
+{
+
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * x;
+
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
---
+++ GG_Robot/Assets/Scripts/Trigger_New_Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_New_Level : MonoBehaviour
+{
+    public GameObject nextLevel;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            player.transform.position = nextLevel.transform.position;
+        }
+    }
+}
